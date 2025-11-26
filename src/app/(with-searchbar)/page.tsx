e31a9afea8ea4a98063850dd3de103b2e122c3d2
pyosunyoung <tjsdudtbvj12@naver.com>
2025-11-26 17:36:04 +0900

    레이아웃, 라우팅 설정(App Router)

---
+++ src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import styles from "./page.module.css";
+
+export default function Home() { // 서버 컴포넌트
+  
+  return (
+    
+    <div className={styles.page}>
+      인덱스 페이지
+    </div>
+  );
+}
